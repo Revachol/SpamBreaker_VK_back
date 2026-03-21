3e9fe552f2bc95375d6cce1fdf5439d169c19a05
ender019 <gilkr125@gmail.com>
2026-03-21 17:56:55 +0300

    postgres connect

--- config/config.go
+++ config/config.go
@@ -2,11 +2,13 @@ package config
 
 import (
 	"os"
+	"time"
 )
 
 type Config struct {
-	Server ServerConfig
-	ML     MLConfig
+	Server   ServerConfig
+	ML       MLConfig
+	Postgres PostgresConfig
 }
 
 type ServerConfig struct {
@@ -17,6 +19,22 @@ type MLConfig struct {
 	BaseURL string
 }
 
+type PostgresConfig struct {
+	Host                string        `yaml:"host"`
+	Port                string        `yaml:"port"`
+	Base                string        `yaml:"base"`
+	User                string        `yaml:"user"`
+	Password            string        `yaml:"password"`
+	MinConns            int32         `yaml:"min_conns"`
+	MaxConns            int32         `yaml:"max_conns"`
+	MaxLife             time.Duration `yaml:"max_life"`
+	MaxIdle             time.Duration `yaml:"max_idle"`
+	HealthCheckInterval time.Duration `yaml:"health_check_interval"`
+
+	Migrated   bool   `yaml:"migrated"`
+	Migrations string `yaml:"migrations"`
+}
+
 func Load() *Config {
 	return &Config{
 		Server: ServerConfig{
@@ -24,6 +42,21 @@ func Load() *Config {
 		},
 		ML: MLConfig{
 			BaseURL: getEnv("ML_SERVICE_URL", "http://localhost:8000"),
+		},
+		Postgres: PostgresConfig{
+			Host:                getEnv("PG_HOST", "localhost"),
+			Port:                getEnv("PG_PORT", "5432"),
+			Base:                getEnv("PG_BASE", "spambreaker"),
+			User:                getEnv("PG_USER", "uservice"),
+			Password:            getEnv("PG_PSWD", "password"),
+			MinConns:            1,
+			MaxConns:            10,
+			MaxLife:             time.Hour,
+			MaxIdle:             30 * time.Minute,
+			HealthCheckInterval: 1 * time.Minute,
+
+			Migrated:   false,
+			Migrations: "./infra/migrations/",
 		},
 	}
 }
