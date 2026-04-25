3629ed4c06ba8163c17c10c5351c1cb95ae1378a
ender019 <gilkr125@gmail.com>
2026-04-25 19:21:17 +0300

    hotfix

--- internal/bots/vk/handlers/handler.go
+++ internal/bots/vk/handlers/handler.go
@@ -76,7 +76,11 @@ func (b *VKBot) handleMessage(msg *object.MessagesMessage) error {
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 		active, err := b.client.IsChatActive(ctx, peerIDStr)
 		cancel()
-		if err != nil || !active {
+		if err != nil {
+			b.logger.Warnf("failed to check chat %s registration: %v", peerIDStr, err)
+			return nil
+		}
+		if !active {
 			return nil
 		}
 	}
